b4fef75a84e6ca9607c81ab5786bec348f19bade
mithas92 <73435940+mithas92@users.noreply.github.com>
2025-04-26 09:33:52 -0500

    Slower screen

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         
         for event in pygame.event.get():
@@ -19,6 +22,8 @@ def main():
        
         screen.fill(color="black")
         pygame.display.flip()
+    
+    dt = game_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
