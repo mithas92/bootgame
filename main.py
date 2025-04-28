53bdc6b30bae3e684471db494cc2f19ef37e6835
mithas92 <73435940+mithas92@users.noreply.github.com>
2025-04-28 11:59:39 -0500

    Draw player

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # imports
 import pygame
 from constants import *
+from player import Player
+
+
+
+# Base class for game objects
 
 def main():
     print("Starting Asteroids!")
@@ -14,6 +19,8 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         
         for event in pygame.event.get():
@@ -21,6 +28,7 @@ def main():
                 return
        
         screen.fill(color="black")
+        player1.draw(screen)
         pygame.display.flip()
     
     dt = game_clock.tick(60)/1000
